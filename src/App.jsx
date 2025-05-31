1257276d18be383d6b50d1dacc83eb0b365a7fcc
CM Mubeen <cmmubeen@gmail.com>
2025-05-31 06:13:59 +0500

    create function to generate password

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 function App() {
   const [length, setLength] = useState(8)
   const [numAllowed, setNumAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
-  
+
+  const generatePassword = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (numAllowed) str += "0123456789"
+    if (charAllowed) str += "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\\"
+
+    for (let i = 1; i < length; i++) {
+      const index = Math.floor(Math.random() * str.length + 1)
+      console.log(index)
+      pass += str.charAt(index)
+    }
+    setPassword(pass)
+  }, [length, numAllowed, charAllowed, password])
+
+  useEffect(() => {
+    generatePassword
+  }, [length, numAllowed, charAllowed])
+
   return (
     <>
       <div className="flex justify-center mt-9">
@@ -18,12 +37,21 @@ function App() {
           </div>
           {/* slider, length, Numbers checkbox, Charactors checkbox */}
           <div className="flex items-center justify-center text-orange-500 font-semibold mt-3">
-            <input type="range" min={8} max={20} onChange={(e) => setLength(e.target.value)}/>
+            {/* slider */}
+            <input type="range" min={8} max={20} onChange={(e) => setLength(e.target.value)} />
             <label htmlFor="slider" className="ml-3">Length: {length}</label>
-            <input type="checkbox" className="ml-3 mr-2" />
-            <label htmlFor="numbers" >Numbers</label>
-            <input type="checkbox" className="ml-3 mr-2" />
-            <label htmlFor="chars">Charactors</label>
+
+            {/* checkbox for numbers */}
+            <input type="checkbox" className="ml-3 mr-2" defaultChecked={numAllowed} onChange={() => {
+              setNumAllowed((prev) => !prev)
+            }} />
+            <label htmlFor="number" >Numbers</label>
+
+            {/* checkbox for charactor */}
+            <input type="checkbox" className="ml-3 mr-2" defaultChecked={charAllowed} onChange={() => {
+              setCharAllowed((prev) => !prev)
+            }} />
+            <label htmlFor="charactor" >Charactors</label>
           </div>
         </div>
       </div>
